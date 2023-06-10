e1374f3f0397257841c2b158da2d96a1fb6d7921
Datacode <46746106+akshayDatacode@users.noreply.github.com>
2023-06-10 16:10:19 +0530

    Update index.js Passcode Updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to DATABASE
-const dbUrl =  "mongodb://127.0.0.1:27017/subscribers";
+const dbUrl =  "mongodb+srv://latamanwani:Lata@12345@subscribers.v1keo1i.mongodb.net/?retryWrites=true&w=majority";
 mongoose
   .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
